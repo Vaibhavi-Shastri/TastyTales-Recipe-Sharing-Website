35489f7d91294ed3f1a44d77097e8428760c2118
Vaibhavi-Shastri <164651148+Vaibhavi-Shastri@users.noreply.github.com>
2024-12-07 12:07:45 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,27 +3,25 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const protect = async (req, res, next) => {
-  try {
-      console.log('Authorization header:', req.headers.authorization);
-
-      if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer')) {
-          return res.status(401).json({ message: 'Not authorized, no token' });
+  
+  if (req.headers.authorization) {
+    let token = req.headers.authorization;
+    try {  
+      if(token){
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select('-password');
+        next();
       }
-
-      const token = req.headers.authorization.split(' ')[1];
-      console.log('Extracted token:', token);
-
-      // Verify token and attach user to the request object
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded;
-
-      console.log('Decoded user:', req.user);
-      next();
-  } catch (error) {
-      console.error('Error in protect middleware:', error);
-      res.status(401).json({ message: 'Not authorized, token failed' });
+      else{
+        return res.sendStatus(403).json({ message: 'Not authorized, Please login' });;
+      }
+    } catch (error) {
+      return res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  }
+  else{
+    res.sendStatus(401).json({ message: 'Not authorized, token failed' });;
   }
 };
-
 
 module.exports = { protect };
